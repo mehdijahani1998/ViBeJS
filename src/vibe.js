73c0bd25202680d18d5b5e11938e8202cfdb09e8
Mehdi Jahani <80170150+mehdijahani1998@users.noreply.github.com>
2025-10-28 16:20:03 -0700

    Refactor vibe class for margin handling and method calls
    Updated width and height calculations to use margins. Refactored methods for clarity and added calls to setData and addMarks.

--- src/vibe.js
+++ src/vibe.js
@@ -4,52 +4,44 @@ export class vibe {
   constructor(xDomain, yDomain, width, height, margins) {
     this.xDomain = xDomain;
     this.yDomain = yDomain;
-    this.width = width - 40; // Subtracting margins from width
-    this.height = height - 40; // Subtracting margins from height
+    this.width = width - margins.left - margins.right;
+    this.height = height - margins.top - margins.bottom;
     this.margins = margins;
-    this.setData();
-    // Create an SVG element with appropriate margins
-    this.svg = d3.create("svg").attr("width", width).attr("height", height);
-    this.g = this.svg
-      .append("g")
-      .attr("transform", `translate(${this.margins.left},${this.margins.top})`);
-    this.highlightG = this.svg
-      .append("g")
-      .attr("transform", `translate(${this.margins.left},${this.margins.top})`);
-    // invisible rectangle to register hover events but not register click events
 
-    return this;
+    this.setData(); // ← NOW CALLED
+
+    this.svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    this.g = this.svg.append("g")
+      .attr("transform", `translate(${margins.left},${margins.top})`);
+
+    this.highlightG = this.svg.append("g")
+      .attr("transform", `translate(${margins.left},${margins.top})`);
   }
 
-  // xDomain is an array of strings
-  // yDomain is an array with minimum and maximum values of input domain
   setData() {
-    // Data initialization
-    this.data = xDomain.map((d) => ({ x: d, y: 0 }));
+    // Default: zero-filled data
+    this.data = this.xDomain.map(d => ({ x: d, y: 0 }));
   }
 
-  // set scale based on xDomain and yDomain
-  setScale() {}
-
-  // create axis based on xScale and yScale
-  createAxis() {}
-
-  // creates event on svg to draw elements
-  addDrawEvent() {}
-
-  // adds marks when data is updated
-  addMarks() {}
+  setScales() { /* override */ }
+  createAxis() { /* override */ }
+  addDrawEvent() { /* override */ }
+  addHighlightEvent() { /* override */ }
+  addMarks() { /* override */ }
 
   getData() {
     return this.data;
   }
 
-  // creates the chart and returns svg node, should call setScales, createAxis, addDrawEvent, addHighlightEvent
   create() {
     this.setScales();
     this.createAxis();
     this.addDrawEvent();
     this.addHighlightEvent();
+    this.addMarks(); // ← NOW IN BASE
     return this.svg.node();
   }
 }
